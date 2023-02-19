f34c3de43a65685690229408b912a0006a7416ec
robles153 <marcoskim153@gmail.com>
2023-02-19 11:31:01 -0300

    Criando Modelos de entidade

---
+++ Lanches/Models/Lanche.cs
@@ -0,0 +1,18 @@
+﻿namespace Lanches.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Name { get; set; }    
+        public string DescricaoCurta { get; set; }
+        public string Descricaodetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get;set; }
+        public string ImagemThumbnaiUrl { get;set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+
+    }
+}
