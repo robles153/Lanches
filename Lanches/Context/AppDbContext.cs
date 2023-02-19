616172bc6a1f351e257bcbd79de365194be2aa50
robles153 <marcoskim153@gmail.com>
2023-02-19 14:20:03 -0300

    criando classe de context

---
+++ Lanches/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Lanches.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {            
+        }
+
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
