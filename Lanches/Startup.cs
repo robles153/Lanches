c53f85949eea1092fd1b86c368b304b81b83f047
robles153 <marcoskim153@gmail.com>
2023-02-19 14:21:03 -0300

    Adicionando serciço de DbContext

--- Lanches/Startup.cs
+++ Lanches/Startup.cs
@@ -1,4 +1,7 @@
-﻿namespace Lanches;
+﻿using Lanches.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lanches;
 
 public class Startup
 {
@@ -12,6 +15,9 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddDbContext<AppDbContext>(options =>
+             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
         services.AddControllersWithViews();
     }
 
