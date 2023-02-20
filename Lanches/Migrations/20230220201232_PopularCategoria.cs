4ffbf4bba127a12c0eff5d8b42b872efb005daad
robles153 <marcoskim153@gmail.com>
2023-02-20 17:21:54 -0300

    Migração pra popular caregoria

---
+++ Lanches/Migrations/20230220201232_PopularCategoria.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lanches.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) " +
+                "VALUES('Normal', 'Lanche feito com ingredientes normais')");
+            mb.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) " +
+              "VALUES('Natural', 'Lanche feito com ingredientes naturais')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Categorias");
+        }
+    }
+}
