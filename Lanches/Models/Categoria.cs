6c5185fbd4f0ff84820357d5f07160d00b3b49e8
robles153 <marcoskim153@gmail.com>
2023-02-20 12:10:46 -0300

    Corrigindo anotação de descrição

--- Lanches/Models/Categoria.cs
+++ Lanches/Models/Categoria.cs
@@ -8,14 +8,14 @@ namespace Lanches.Models
     {
         [Key]
         public int CategoriaId { get; set; }
-        [StringLength(100,ErrorMessage ="Otamanho maximo é 100 caracters")]
+        [StringLength(100,ErrorMessage ="O tamanho maximo é 100 caracters")]
         [Required(ErrorMessage ="Informe o Nome da Categoria")]
         [Display(Name ="Nome")]
         public string CategoriaNome { get; set; }
         
-        [StringLength(200, ErrorMessage = "Otamanho maximo é 100 caracters")]
+        [StringLength(200, ErrorMessage = "O tamanho maximo é 100 caracters")]
         [Required(ErrorMessage = "Informe a descrição da Categoria")]
-        [Display(Name = "Nome")]
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
         public List<Lanche> Lanches { get; set; }
 
