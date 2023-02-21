d605922c3cc5cc7293c0960e24068700383ec302
robles153 <marcoskim153@gmail.com>
2023-02-21 13:28:40 -0300

    Registrando os serviços dos repitorios: Injeção de dependencia

--- Lanches/Startup.cs
+++ Lanches/Startup.cs
@@ -1,4 +1,6 @@
 ﻿using Lanches.Context;
+using Lanches.Repositories;
+using Lanches.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Lanches;
@@ -17,6 +19,9 @@ public class Startup
     {
         services.AddDbContext<AppDbContext>(options =>
              options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddTransient<ILancheRepository, LancheRepository>();
+        services.AddTransient<ICategoriaRepository, CategoriaRepository>();
 
         services.AddControllersWithViews();
     }
