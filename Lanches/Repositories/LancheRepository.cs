87be8321ce708a93d375e6b5450303baa9840469
robles153 <marcoskim153@gmail.com>
2023-02-21 13:04:17 -0300

    Criando Repositorios e interfaces para Categoria e Lanche

---
+++ Lanches/Repositories/LancheRepository.cs
@@ -0,0 +1,27 @@
+﻿using Lanches.Context;
+using Lanches.Models;
+using Lanches.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lanches.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _Context;
+        public LancheRepository(AppDbContext context)
+        {
+            _Context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _Context.Lanches.Include(c => c.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _Context.Lanches.
+                                   Where(l => l.IsLanchePreferido).
+                                   Include(c => c.Categoria);
+
+        public Lanche GetLancheById(int id)
+        {
+            return _Context.Lanches.FirstOrDefault(l => l.LancheId== id);
+        }
+    }
+}
