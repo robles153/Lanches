88640ffea5a7be4f561ccc81b441ddb9e4ee7f77
robles153 <marcoskim153@gmail.com>
2023-02-26 13:27:35 -0300

    Implementando metodo GetCarrinho  na classe carrinhoCompra

---
+++ Lanches/Models/CarrinhoCompra.cs
@@ -0,0 +1,39 @@
+﻿using Lanches.Context;
+
+namespace Lanches.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraitem { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            //define uma sessão
+            ISession session =
+                services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //otem um serviço do tipo do nosso sontexto
+            var context = services.GetService<AppDbContext>();
+
+            //otem ou gera o Id do carrinho
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            //atribui o id do carrinho na sessão
+            session.SetString("CarrinhoId", carrinhoId);
+
+            //retorna o carrinho com o contexto e o Id atribuido ou obtido
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }
+    }
+}
