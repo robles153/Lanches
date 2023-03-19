9ac602a818d8cd698e27bf4b46e18ba19d061ab9
robles153 <marcoskim153@gmail.com>
2023-03-19 12:15:04 -0300

    Implementado classe CarrinhoCompraresumo

---
+++ Lanches/Components/CarrinhoComprarRsumo.cs
@@ -0,0 +1,39 @@
+﻿using Lanches.Models;
+using Lanches.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.Components
+{
+    public class CarrinhoComprarRsumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoComprarRsumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke() 
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+
+           /* var itens = new List<CarrinhoCompraItem>()
+            {
+                new CarrinhoCompraItem(),
+                new CarrinhoCompraItem()
+
+            };*/
+
+            _carrinhoCompra.CarrinhoCompraitems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetcarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+
+        }
+    }
+}
